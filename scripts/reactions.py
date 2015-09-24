bcb84a5b85259644ec0949f820ac1ed8f03d1676
Eric Walker <eric@close.co>
2015-09-24 11:02:26 -0500

    Add ability to print out studies view from command line.

--- scripts/reactions.py
+++ scripts/reactions.py
@@ -1,17 +1,21 @@
 import argparse
 
 from lenrmc.nubase import System
-from lenrmc.terminal import TerminalView
+from lenrmc.terminal import TerminalView, StudiesTerminalView
 
 
 class App(object):
 
     def __init__(self, **kwargs):
         self.kwargs = kwargs
+        if 'studies' == self.kwargs.get('view'):
+            self.view_cls = StudiesTerminalView
+        else:
+            self.view_cls = TerminalView
 
     def run(self):
         s = System.parse(self.kwargs['system'], **self.kwargs)
-        for line in TerminalView(s).lines(**self.kwargs):
+        for line in self.view_cls(s).lines(**self.kwargs):
             print(line)
 
 
@@ -21,7 +25,13 @@ def parse_arguments():
     parser.add_argument('--lb', dest='lower_bound')
     parser.add_argument('--spins', dest='spins', action='store_true')
     parser.add_argument('--references', dest='references', action='store_true')
-    parser.set_defaults(lower_bound=0, spins=False, references=True)
+    parser.add_argument('--view', type=str, dest='view')
+    parser.set_defaults(
+        lower_bound = 0,
+        spins       = False,
+        references  = True,
+        view        = 'default',
+    )
     return parser.parse_args()
 
 
